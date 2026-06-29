8be945a2ee76e085afbac3f444698c879420c50b
3900quangtri <3900quangtri@gmail.com>
2026-06-29 16:26:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,16 +7,16 @@ import { executeGenerateQr, executeVerifyAndPayChange } from "./app.js";
 // 2. CẤU HÌNH NGÂN HÀNG ĐÍCH & FIREBASE
 // ==========================================================================
 const BANK_BIN = "970405"; // Mã định danh BIN của Agribank
-const BANK_ACCOUNT = "3900205361940"; 
+const BANK_ACCOUNT = "3902201013072"; 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDabRjEULSZ3U_1hu_JNw9yf_kK-M_sgjM",
-  authDomain: "testhd-85525.firebaseapp.com",
-  databaseURL: "https://testhd-85525-default-rtdb.firebaseio.com",
-  projectId: "testhd-85525",
-  storageBucket: "testhd-85525.firebasestorage.app",
-  messagingSenderId: "609166109513",
-  appId: "1:609166109513:web:cfbc73bc9268e3509b8898"
+    apiKey: "AIzaSyAOSKLNPXp-s40iJNYYzdEWDnQDFoa6x_Q",
+    authDomain: "thue2026-f558d.firebaseapp.com",
+    databaseURL: "https://thue2026-f558d-default-rtdb.firebaseio.com",
+    projectId: "thue2026-f558d",
+    storageBucket: "thue2026-f558d.firebasestorage.app",
+    messagingSenderId: "1008017359572",
+    appId: "1:1008017359572:web:f70cf40778e600e8deb141"
 };
 
 // Khởi tạo Firebase nếu chưa có
@@ -103,7 +103,7 @@ function fetchTaxData() {
                 for (let id in data) {
                     let item = data[id];
                     if (!item.ID) item.ID = id; 
-                    
+						item.id_key = id;
                     if (item.Branch === currentUser.Branch || item.BranchCode === currentUser.Branch) {
                         allData.push(item);
                     }
